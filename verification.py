bd4965baf3bf1df59f1e708dde7c415207fe79b5
Jerome Kelleher <jk@well.ox.ac.uk>
2014-10-03 14:51:33 +0100

    Added ms source.

---
+++ verification.py
@@ -0,0 +1,16 @@
+"""
+Script to automate verification of the msprime simulator against
+Hudson's ms.
+"""
+from __future__ import print_function
+from __future__ import division
+
+import random
+import msprime
+
+
+def main():
+    print("running verification")
+
+if __name__ == "__main__":
+    main()
