b416641256dc6e48b369da94331a348d5929b4c5
Jerome Kelleher <jk@well.ox.ac.uk>
2014-10-24 11:04:23 +0100

    Added some low level tests and accessors.

---
+++ tests/__init__.py
@@ -0,0 +1,62 @@
+"""
+Common code for the msprime test cases.
+"""
+from __future__ import print_function
+from __future__ import division
+
+import os
+import tempfile
+import unittest
+
+class MsprimeTestCase(unittest.TestCase):
+    """
+    Superclass of all tests msprime simulator test cases.
+    """
+
+    def setUp(self):
+        fd, tf = tempfile.mkstemp(prefix="msp_test_", suffix=".dat")
+        os.close(fd)
+        self._treefile = tf
+
+    def tearDown(self):
+        os.unlink(self._treefile)
+
+    def verify_tree(self, n, pi, tau):
+        """
+        Verifies that the specified tree is a consistent coalescent history
+        for a sample of size n.
+        """
+        self.assertEqual(len(pi), 2 * n)
+        self.assertEqual(len(tau), 2 * n)
+        # leading value must be zero.
+        self.assertEqual(pi[0], 0)
+        self.assertEqual(tau[0], 0)
+        num_children = [0 for j in range(0, 2 * n)]
+        for j in range(1, 2 * n):
+            num_children[pi[j]] += 1
+        self.assertEqual(num_children[0], 1)
+        # nodes 1 to n are leaves.
+        for j in range(1, n + 1):
+            self.assertNotEqual(pi[j], 0)
+            self.assertEqual(tau[j], 0)
+            self.assertEqual(num_children[j], 0)
+        # All non-leaf nodes should be binary with non-zero times.
+        for j in range(n + 1, 2 * n):
+            self.assertEqual(num_children[j], 2)
+            self.assertGreater(tau[j], 0.0)
+        # times of non leaves should be distinct and increasing.
+        taup = [tau[j] for j in range(n + 1, 2 * n)]
+        self.assertEqual(len(set(taup)), len(taup))
+        self.assertEqual(taup, sorted(taup))
+
+    def verify_trees(self, n, m, trees):
+        """
+        Verifies that the specified set of trees is consistent with the specified
+        paramters.
+        """
+        s = 0
+        for l, pi, tau in trees:
+            self.verify_tree(n, pi, tau)
+            self.assertTrue(l > 0)
+            s += l
+        self.assertEqual(s, m)
--- tests/test_lowlevel.py
+++ tests/test_lowlevel.py
@@ -5,7 +5,86 @@ from __future__ import print_function
 from __future__ import division
 
 import random
+
+import tests
 import _msprime
 
-def test_stuff():
-    assert True
+class TestInterface(tests.MsprimeTestCase):
+    """
+    Test the low-level interface to make sure it is robust.
+    """
+    def verify_random_paramters(self):
+        mb = 1024 * 1024
+        n = random.randint(2, 1000)
+        m = random.randint(1, 10**6)
+        r = random.uniform(0, 1000)
+        models = []
+        random_seed = random.randint(0, 2**31)
+        max_memory = random.randint(10 * mb, 100 * mb)
+        segment_block_size = random.randint(1, 100)
+        node_mapping_block_size = random.randint(1, 100)
+        avl_node_block_size = random.randint(1, 100)
+        sim = _msprime.Simulator(sample_size=n, num_loci=m,
+                population_models=models, scaled_recombination_rate=r,
+                random_seed=random_seed, tree_file_name=self._treefile,
+                max_memory=max_memory, segment_block_size=segment_block_size,
+                avl_node_block_size=avl_node_block_size,
+                node_mapping_block_size=node_mapping_block_size)
+        # Check the getters to ensure we've got the right values.
+        self.assertEqual(n, sim.get_sample_size())
+        self.assertEqual(m, sim.get_num_loci())
+        self.assertEqual(r, sim.get_scaled_recombination_rate())
+        self.assertEqual(random_seed, sim.get_random_seed())
+        self.assertEqual(self._treefile, sim.get_tree_file_name())
+        # TODO fix population models!
+        # self.assertEqual(models, sim.get_population_models())
+        self.assertEqual(max_memory, sim.get_max_memory())
+        self.assertEqual(segment_block_size, sim.get_segment_block_size())
+        self.assertEqual(avl_node_block_size, sim.get_avl_node_block_size())
+        self.assertEqual(node_mapping_block_size, sim.get_node_mapping_block_size())
+        # Check initial state
+        self.assertEqual(1, sim.get_num_trees())
+        self.assertEqual(0.0, sim.get_time())
+        self.assertEqual(n, sim.get_num_ancestors())
+        self.assertEqual(0, sim.get_num_coancestry_events())
+        self.assertEqual(0, sim.get_num_recombination_events())
+        self.assertGreater(sim.get_num_avl_node_blocks(), 0)
+        self.assertGreater(sim.get_num_segment_blocks(), 0)
+        self.assertGreater(sim.get_num_node_mapping_blocks(), 0)
+        a = 0
+        nodes = set()
+        for ind in sim.get_ancestors():
+            self.assertEqual(len(ind), 1)
+            l, r, node  = ind[0]
+            self.assertEqual(l, 1)
+            self.assertEqual(r, m)
+            self.assertFalse(node in nodes)
+            nodes.add(node)
+            a += 1
+        self.assertEqual(a, n)
+
+    def verify_simulation(self, n, m, r, models):
+        """
+        Runs the specified simulation and verifies its state.
+        """
+        mb = 1024 * 1024
+        random_seed = random.randint(0, 2**31)
+        sim = _msprime.Simulator(sample_size=n, num_loci=m,
+                population_models=models, scaled_recombination_rate=r,
+                random_seed=random_seed, tree_file_name=self._treefile,
+                max_memory=10 * mb, segment_block_size=1000,
+                avl_node_block_size=1000, node_mapping_block_size=1000)
+        # Run the sim for a tiny amount of time and check.
+        # BUG here!
+        # self.assertFalse(sim.run(1e-8))
+
+        # Now run until coalescence
+        self.assertTrue(sim.run())
+
+    def test_random_sims(self):
+        num_random_sims = 10
+        for j in range(num_random_sims):
+            self.verify_random_paramters()
+
+    def test_small_sims(self):
+        self.verify_simulation(10, 100, 1.0, [])
