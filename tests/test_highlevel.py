6bb5fa6c6a2783808e108ef89ff9bfa55f4d1e8d
Jerome Kelleher <jk@well.ox.ac.uk>
2015-05-20 15:47:23 +0100

    Try to isolate Travis problem.

--- tests/test_highlevel.py
+++ tests/test_highlevel.py
@@ -13,6 +13,8 @@ import msprime
 
 import tests
 
+# TMP
+from nose.tools import nottest
 
 def sparse_tree_to_newick(pi, tau, precision):
     """
@@ -322,6 +324,7 @@ class TestHaplotypeGenerator(HighLevelTestCase):
             self.assertEqual(len(h), tree_sequence.get_num_mutations())
         self.verify_haplotypes(n, haplotypes)
 
+    @nottest
     def test_random_parameters(self):
         num_random_sims = 10
         for j in range(num_random_sims):
