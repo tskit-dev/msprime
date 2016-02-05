80a23fdfe7a604f94f23581c1f92160c9ee765f2
Jerome Kelleher <jk@well.ox.ac.uk>
2016-02-05 12:32:57 +0000

    Set random_seed for stress tester.

--- stress_lowlevel.py
+++ stress_lowlevel.py
@@ -6,18 +6,22 @@ from __future__ import print_function
 from __future__ import division
 
 import unittest
+import random
 
 import tests.test_lowlevel as test_lowlevel
 import tests.test_highlevel as test_highlevel
 
 
 def main():
-    testloader = unittest.TestLoader()
-    test_lowlevel.enable_h5py_tests = False
-    suite = testloader.loadTestsFromModule(test_lowlevel)
-    l = testloader.loadTestsFromModule(test_highlevel)
-    suite.addTests(l)
     while True:
+        # We don't want any random variation in the amount of memory
+        # used from test-to-test.
+        random.seed(1)
+        testloader = unittest.TestLoader()
+        test_lowlevel.enable_h5py_tests = False
+        suite = testloader.loadTestsFromModule(test_lowlevel)
+        l = testloader.loadTestsFromModule(test_highlevel)
+        suite.addTests(l)
         unittest.TextTestRunner(verbosity=0).run(suite)
 
 
