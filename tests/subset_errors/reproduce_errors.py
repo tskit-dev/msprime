tskit-dev/msprime

commit 4a87ead476c672f216109bb5f8f52409c9956c21

Added high-level interface and tests.

_msprimemodule.c
@@ -246,6 +246,50 @@ out:
 }
 
 static int
+parse_sample_ids(PyObject *py_samples, tree_sequence_t *ts, size_t *num_samples,
+        uint32_t **samples)
+{
+    int ret = -1;
+    PyObject *item;
+    size_t j;
+    Py_ssize_t num_samples_local;
+    uint32_t *samples_local = NULL;
+    uint32_t n = tree_sequence_get_sample_size(ts);
+
+    num_samples_local = PyList_Size(py_samples);
+    if (num_samples_local < 2) {
+        PyErr_SetString(PyExc_ValueError, "Must provide at least 2 samples");
+        goto out;
+    }
+    samples_local = PyMem_Malloc(num_samples_local * sizeof(uint32_t));
+    if (samples_local == NULL) {
+        PyErr_NoMemory();
+        goto out;
+    }
+    for (j = 0; j < num_samples_local; j++) {
+        item = PyList_GetItem(py_samples, j);
+        if (!PyNumber_Check(item)) {
+            PyErr_SetString(PyExc_TypeError, "sample id must be a number");
+            goto out;
+        }
+        samples_local[j] = (uint32_t) PyLong_AsLong(item);
+        if (samples_local[j] >= n) {
+            PyErr_SetString(PyExc_ValueError, "sample ids must be < sample_size");
+            goto out;
+        }
+    }
+    *num_samples = (size_t) num_samples_local;
+    *samples = samples_local;
+    samples_local = NULL;
+    ret = 0;
+out:
+    if (samples_local != NULL) {
+        PyMem_Free(samples_local);
+    }
+    return ret;
+}
+
+static int
 parse_samples(PyObject *py_samples, Py_ssize_t *sample_size,
         sample_t **samples)
 {
@@ -2825,11 +2869,9 @@ TreeSequence_get_pairwise_diversity(TreeSequence *self, PyObject *args,
 {
     PyObject *ret = NULL;
     PyObject *py_samples = NULL;
-    PyObject *item;
     static char *kwlist[] = {"samples", NULL};
     uint32_t *samples = NULL;
     size_t num_samples = 0;
-    uint32_t j, n;
     double pi;
     int err;
 
@@ -2840,29 +2882,8 @@ TreeSequence_get_pairwise_diversity(TreeSequence *self, PyObject *args,
             &PyList_Type, &py_samples)) {
         goto out;
     }
-    n = tree_sequence_get_sample_size(self->tree_sequence);
-    num_samples = PyList_Size(py_samples);
-    if (num_samples < 2) {
-        PyErr_SetString(PyExc_ValueError, "Must provide at least 2 samples");
+    if (parse_sample_ids(py_samples, self->tree_sequence, &num_samples, &samples) != 0) {
         goto out;
-    }
-    samples = PyMem_Malloc(num_samples * sizeof(uint32_t));
-    if (samples == NULL) {
-        PyErr_NoMemory();
-        goto out;
-    }
-    for (j = 0; j < num_samples; j++) {
-        item = PyList_GetItem(py_samples, j);
-        if (!PyNumber_Check(item)) {
-            PyErr_SetString(PyExc_TypeError, "sample id must be a number");
-            goto out;
-        }
-        samples[j] = (uint32_t) PyLong_AsLong(item);
-        if (samples[j] >= n) {
-            PyErr_SetString(PyExc_ValueError,
-                    "sample ids must be < sample_size");
-            goto out;
-        }
     }
     err = tree_sequence_get_pairwise_diversity(
         self->tree_sequence, samples, (uint32_t) num_samples, &pi);
@@ -2876,6 +2897,58 @@ out:
         PyMem_Free(samples);
     }
 
+    return ret;
+}
+
+/* Forward declaration */
+static PyObject * build_TreeSequence(tree_sequence_t *ts);
+static PyObject *
+TreeSequence_get_subset(TreeSequence *self, PyObject *args, PyObject *kwds)
+{
+    PyObject *ret = NULL;
+    PyObject *py_samples = NULL;
+    static char *kwlist[] = {"samples", NULL};
+    uint32_t *samples = NULL;
+    size_t num_samples = 0;
+    tree_sequence_t *subset_ts = NULL;
+    int err;
+
+    if (TreeSequence_check_tree_sequence(self) != 0) {
+        goto out;
+    }
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!", kwlist,
+            &PyList_Type, &py_samples)) {
+        goto out;
+    }
+    if (parse_sample_ids(py_samples, self->tree_sequence, &num_samples, &samples) != 0) {
+        goto out;
+    }
+    subset_ts = PyMem_Malloc(sizeof(tree_sequence_t));
+    if (subset_ts == NULL) {
+        PyErr_NoMemory();
+        goto out;
+    }
+    memset(subset_ts, 0, sizeof(tree_sequence_t));
+    err = tree_sequence_get_subset(
+        self->tree_sequence, samples, (uint32_t) num_samples, subset_ts);
+    if (err != 0) {
+        handle_library_error(err);
+        goto out;
+    }
+    ret = build_TreeSequence(subset_ts);
+    if (ret != NULL) {
+        /* the new TreeSequence object now has ownership of the tree
+         * sequence so we must not free it */
+        subset_ts = NULL;
+    }
+out:
+    if (samples != NULL) {
+        PyMem_Free(samples);
+    }
+    if (subset_ts != NULL) {
+        tree_sequence_free(subset_ts);
+        PyMem_Free(subset_ts);
+    }
     return ret;
 }
 
@@ -2931,7 +3004,7 @@ static PyMethodDef TreeSequence_methods[] = {
     {"get_sequence_length", (PyCFunction) TreeSequence_get_sequence_length,
         METH_NOARGS, "Returns the sequence length in bases." },
     {"get_num_mutations", (PyCFunction) TreeSequence_get_num_mutations, METH_NOARGS,
-        "Returns the number of loci" },
+        "Returns the number of mutations" },
     {"get_num_nodes", (PyCFunction) TreeSequence_get_num_nodes, METH_NOARGS,
         "Returns the number of unique nodes in the tree sequence." },
     {"get_sample_size", (PyCFunction) TreeSequence_get_sample_size, METH_NOARGS,
@@ -2941,6 +3014,8 @@ static PyMethodDef TreeSequence_methods[] = {
     {"get_pairwise_diversity",
         (PyCFunction) TreeSequence_get_pairwise_diversity,
         METH_VARARGS|METH_KEYWORDS, "Returns the average pairwise diversity." },
+    {"get_subset", (PyCFunction) TreeSequence_get_subset,
+        METH_VARARGS|METH_KEYWORDS, "Returns a subset of this TreeSequence." },
     {NULL}  /* Sentinel */
 };
 
@@ -2982,6 +3057,22 @@ static PyTypeObject TreeSequenceType = {
     0,                         /* tp_dictoffset */
     (initproc)TreeSequence_init,      /* tp_init */
 };
+
+static PyObject *
+build_TreeSequence(tree_sequence_t *ts)
+{
+    PyObject *ret = NULL;
+    TreeSequence *new_ts = NULL;
+
+    new_ts = (TreeSequence *) PyObject_CallObject((PyObject *) &TreeSequenceType, NULL);
+    if (new_ts == NULL) {
+        goto out;
+    }
+    new_ts->tree_sequence = ts;
+    ret = (PyObject *) new_ts;
+out:
+    return ret;
+}
 
 /*===================================================================
  * SparseTree

dev.py
@@ -905,17 +905,19 @@ def get_subset_children(pi, samples):
 
 def subset_samples(n, samples):
     demographic_events=[msprime.SimpleBottleneck(1000, 0.15)]
-    # demographic_events = []
-    # ts = msprime.simulate(
-    #     sample_size=n, Ne=1e4, length=1e4, recombination_rate=5e-8,
-    #     mutation_rate=2e-8, random_seed=5, demographic_events=demographic_events)
-    ts = msprime.load(sys.argv[1])
+    demographic_events = []
+    ts = msprime.simulate(
+        sample_size=n, Ne=1e4, length=1e4, recombination_rate=5e-8,
+        mutation_rate=2e-8, random_seed=5, demographic_events=demographic_events)
+    # ts = msprime.load(sys.argv[1])
 
-    print("starting subsetting", len(samples))
-    before = time.clock()
     subset = ts.subset(samples)
-    duration = time.clock() - before
-    print("Subsetting done", duration)
+
+#     print("starting subsetting", len(samples))
+#     before = time.clock()
+#     subset = ts.subset(samples)
+#     duration = time.clock() - before
+#     print("Subsetting done", duration)
 
     all_trees = ts.trees()
     full_tree = next(all_trees)
@@ -926,7 +928,6 @@ def subset_samples(n, samples):
                 # print(u, v, samples[u], samples[v])
                 t_mrca1 = full_tree.get_tmrca(samples[u], samples[v])
                 t_mrca2 = subset_tree.get_tmrca(u, v)
-                # print(t_mrca1, t_mrca2, t_mrca1 == t_mrca2)
                 assert t_mrca1 == t_mrca2
             full_tree = next(all_trees, None)
             if full_tree is None:
@@ -968,7 +969,7 @@ if __name__ == "__main__":
     #     subset_samples(30000, list(range(k)))
 
     # subset_samples(30000, [5, 7, 8,9, 10, 11])
-    subset_samples(30000, list(range(500)))
+    subset_samples(300, list(range(20)))
 
     # for n in [100, 1000, 10000]:
     #     for k in [2, 10, 50, n - 1, n]:

msprime/trees.py
@@ -1831,173 +1831,14 @@ class TreeSequence(object):
             output.write(record)
 
     def subset(self, samples):
-        if len(samples) < 2:
-            raise ValueError("Must have at least two samples")
-
-        records = list(self.records())
-        mutations = list(self.mutations())
-        new_records = []
-        new_mutations = []
-        M = len(records)
-        I = sorted(range(M), key=lambda j: (records[j].left, records[j].time))
-        O = sorted(range(M), key=lambda j: (records[j].right, -records[j].time))
-        pi = [-1 for j in range(self.get_num_nodes())]
-        tau = [-1 for j in range(self.get_num_nodes())]
-        kappa = [-1 for j in range(self.get_num_nodes())]
-        mu = [-1 for j in range(self.get_num_nodes())]
-        chi = [[] for _ in range(self.get_num_nodes())]
-        active_records = {}
-        for u in samples:
-            mu[u] = u
-
-        j = 0
-        k = 0
-        l = 0
-        while j < M:
-            start_records = []
-            end_records = []
-            x = records[I[j]].left
-            while records[O[k]].right == x:
-                h = O[k]
-                # print("\tout:", records[h])
-                k += 1
-                for q in records[h].children:
-                    pi[q] = -1
-                u = records[h].node
-                tau[u] = -1
-                kappa[u] = -1
-                chi[u] = []
-                if mu[u] != -1:
-                    if mu[u] == u:
-                        end_records.append(u)
-                    # At least one path goes through u. Follow this path until we either
-                    # hit the root or another path, resetting it to null.
-                    v = mu[u]
-                    while u != -1 and mu[u] == v:
-                        mu[u] = -1
-                        u = pi[u]
-                    if u != -1:
-                        # We have intersected with another path at u, and therefore need
-                        # to terminate a record here since this coalescence no longer
-                        # exists in the subset tree.
-                        end_records.append(u)
-                        w = -1
-                        for v in chi[u]:
-                            if mu[v] != -1:
-                                w = mu[v] if w == -1 else u
-                        if w == u:
-                            # There is still more than one path going through this node
-                            # and so we must start a new record from here.
-                            end_records.append(u)
-                            start_records.append(u)
-                        else:
-                            assert w != -1
-                            # Now propagate this new mapping up the tree until we hit the
-                            # root or another path.
-                            v = mu[u]
-                            while u != -1 and mu[u] == v:
-                                mu[u] = w
-                                u = pi[u]
-                            if u != -1:
-                                # u is an existing coalescence node where the children
-                                # have now changed.
-                                end_records.append(u)
-                                start_records.append(u)
-
-            while j < M and records[I[j]].left == x:
-                h = I[j]
-                j += 1
-                # print("\tin:", records[h])
-                u = records[h].node
-                tau[u] = records[h].time
-                kappa[u] = records[h].population
-                chi[records[h].node] = records[h].children
-                w = -1
-                for v in records[h].children:
-                    pi[v] = u
-                    if mu[v] != -1:
-                        w = mu[v] if w == -1 else u
-                if w != -1:
-                    if w == u:
-                        start_records.append(u)
-                    # There was one or more mapped children in this record, and so we
-                    # must propagate this mapping up until it either intersects with
-                    # another path or we hit root.
-                    while u != -1 and mu[u] == -1:
-                        mu[u] = w
-                        u = pi[u]
-                    if u != -1:
-                        # u is a coalescence node where the new path intersects with
-                        # one or more existing path. Follow the existing path upwards
-                        # until we intersect with another path, setting the mapping to
-                        # its new value.
-                        start_records.append(u)
-                        v = mu[u]
-                        w = u
-                        while u != -1 and mu[u] == v:
-                            mu[u] = w
-                            u = pi[u]
-                        if u != -1:
-                            # u is an existing coalescence node where the children have
-                            # now changed.
-                            end_records.append(u)
-                            start_records.append(u)
-            # Update the actual records now that the trees have been completed.
-            for u in end_records:
-                if u in active_records:
-                    left, children, time, population = active_records.pop(u)
-                    new_records.append(msprime.CoalescenceRecord(
-                        left=left, right=x, node=u, children=sorted(children),
-                        time=time, population=population))
-            for u in start_records:
-                mapped_children = [mu[c] for c in chi[u] if mu[c] != -1]
-                # For some reason there are times when we add nodes to the list
-                # to be updated when they don't need to be. Simple workaround here,
-                # but we should address the root cause.
-                if len(mapped_children) > 1:
-                    active_records[u] = x, mapped_children, tau[u], kappa[u]
-            # update the mutations for this tree.
-            y = records[O[k]].right
-            while l < self.get_num_mutations() and mutations[l].position < y:
-                u = mutations[l].node
-                if mu[u] != -1:
-                    new_mutations.append((mutations[l].position, mu[u]))
-                l += 1
-
-        for u, (left, children, time, population) in active_records.items():
-            new_records.append(msprime.CoalescenceRecord(
-                left=left, right=self.get_sequence_length(), node=u,
-                children=sorted(children), time=time,
-                population=population))
-
-        new_records.sort(key=lambda r: (r.time, r.left))
-        squashed_records = [new_records[0]]
-        for r2 in new_records[1:]:
-            r1 = squashed_records[-1]
-            if r1.right == r2.left and r1.node == r2.node and r1.children == r2.children:
-                squashed_records[-1] = r1._replace(right=r2.right)
-            else:
-                squashed_records.append(r2)
-        # Now compress the records into a new tree sequence.
-        node_map = [-1 for _ in range(self.get_num_nodes())]
-        for j, u in enumerate(samples):
-            node_map[u] = j
-        compressed_records = []
-        next_node = len(samples)
-        for record in squashed_records:
-            for node in list(record.children) + [record.node]:
-                if node_map[node] == -1:
-                    node_map[node] = next_node
-                    next_node += 1
-            children = tuple(sorted(node_map[c] for c in record.children))
-            compressed_records.append(msprime.CoalescenceRecord(
-                left=record.left, right=record.right, node=node_map[record.node],
-                children=children, time=record.time, population=record.population))
-        compressed_mutations = [(pos, node_map[u]) for (pos, u) in new_mutations]
-        ll_ts = _msprime.TreeSequence()
-        ll_ts.load_records(compressed_records)
-        ll_ts.set_mutations(compressed_mutations)
-        return msprime.TreeSequence(ll_ts)
+        ll_subset = self._ll_tree_sequence.get_subset(samples)
+        subset = msprime.TreeSequence(ll_subset)
+        for provenance in self.get_provenance():
+            subset.add_provenance(provenance)
+        parameters = {"TODO": "encode subset parameters"}
+        subset_provenance = get_provenance_dict("subset", parameters)
+        subset.add_provenance(json.dumps(subset_provenance))
+        return subset
 
 
 class HaplotypeGenerator(object):

tests/test_highlevel.py
@@ -127,15 +127,15 @@ def subset_tree_sequence(ts, samples):
     new_records = []
     new_mutations = []
     for tree in ts.trees(tracked_leaves=samples):
-        parent = [-1 for j in range(num_nodes)]
+        parent = [msprime.NULL_NODE for j in range(num_nodes)]
         children = collections.defaultdict(list)
         for leaf in samples:
             u = leaf
             v = tree.get_parent(u)
-            while v != -1:
+            while v != msprime.NULL_NODE:
                 is_parent = (
                     tree.get_num_tracked_leaves(v) > tree.get_num_tracked_leaves(u))
-                if parent[u] == -1 and is_parent:
+                if parent[u] == msprime.NULL_NODE and is_parent:
                     parent[u] = v
                     children[v].append(u)
                     children[v].sort()
@@ -158,13 +158,17 @@ def subset_tree_sequence(ts, samples):
                 new_records.append(msprime.CoalescenceRecord(*active_records[u]))
                 active_records[u][0] = tree.get_interval()[0]
                 active_records[u][3] = tuple(c)
+        subset_root = samples[0]
+        while parent[subset_root] != msprime.NULL_NODE:
+            subset_root = parent[subset_root]
         # Now find the new nodes for all mutations that can be mapped back in
         for mut in tree.mutations():
             stack = [mut.node]
             while not len(stack) == 0:
                 u = stack.pop()
-                if parent[u] != -1 or u in children:
-                    new_mutations.append((mut.position, u))
+                if parent[u] != msprime.NULL_NODE or u in children:
+                    if u != subset_root:
+                        new_mutations.append((mut.position, u))
                     break
                 stack.extend(tree.get_children(u))
     for record in active_records.values():
@@ -173,14 +177,14 @@ def subset_tree_sequence(ts, samples):
     new_records.sort(key=lambda r: r.time)
 
     # Now compress the nodes.
-    node_map = [-1 for _ in range(num_nodes)]
+    node_map = [msprime.NULL_NODE for _ in range(num_nodes)]
     for j, u in enumerate(samples):
         node_map[u] = j
     compressed_records = []
     next_node = len(samples)
     for record in new_records:
         for node in list(record.children) + [record.node]:
-            if node_map[node] == -1:
+            if node_map[node] == msprime.NULL_NODE:
                 node_map[node] = next_node
                 next_node += 1
         children = tuple(sorted(node_map[c] for c in record.children))
@@ -1237,6 +1241,9 @@ class TestTreeSequence(HighLevelTestCase):
         s2 = subset_tree_sequence(ts, sample)
         self.assertEqual(list(s1.records()), list(s2.records()))
         self.assertEqual(list(s1.mutations()), list(s2.mutations()))
+        self.assertEqual(list(s1.haplotypes()), list(s2.haplotypes()))
+        self.assertEqual(
+            list(s1.variants(as_bytes=True)), list(s2.variants(as_bytes=True)))
 
     def test_subset(self):
         num_mutations = 0

tests/test_lowlevel.py
@@ -2019,6 +2019,22 @@ class TestTreeSequence(LowLevelTestCase):
             pi1 = ts.get_pairwise_diversity(samples)
             self.assertGreaterEqual(pi1, 0)
 
+    def test_subset(self):
+        for ts in self.get_example_tree_sequences():
+            for bad_type in ["", None, {}]:
+                self.assertRaises(TypeError, ts.get_subset, bad_type)
+            self.assertRaises(ValueError, ts.get_subset, [])
+            self.assertRaises(ValueError, ts.get_subset, [0])
+            self.assertRaises(ValueError, ts.get_subset, [0, ts.get_sample_size()])
+            self.assertRaises(_msprime.LibraryError, ts.get_subset, [0, 0])
+            s1 = ts.get_subset([0, 1])
+            s2 = ts.get_subset([1, 0])
+            self.assertEqual(ts.get_sequence_length(), s1.get_sequence_length())
+            self.assertEqual(s1.get_sample_size(), s2.get_sample_size())
+            self.assertEqual(s1.get_num_records(), s2.get_num_records())
+            self.assertEqual(s1.get_num_mutations(), s2.get_num_mutations())
+            self.assertEqual(s1.get_num_trees(), s2.get_num_trees())
+
     def test_load_records_equality(self):
         for ts1 in self.get_example_tree_sequences():
             records = [ts1.get_record(j) for j in range(ts1.get_num_records())]

commit bb1105d2323f792e205acb2cd8d9c1fb0f06b408

Early version of subset-> bad records LibraryError
And in subsequent updates these same records will cause more serious errors.

Running the commands below from this commit shows that the change to more
serious error was introduced by `4a87ead` (Added high-level interface and
tests.)

git cherry-pick 2595b
pip install -e . && python tests/subset_errors/reproduce_errors.py
git cherry-pick 4a87ead
pip install -e . && python tests/subset_errors/reproduce_errors.py

NB: the .tsv files commited here contain other records that will cause similar
errors

tests/subset_errors/reproduce_errors.py
@@ -1,0 +1,150 @@
+import pytest
+import tempfile
+import msprime
+from numpy.random import choice
+
+def test_subset():
+    with tempfile.NamedTemporaryFile(mode='w') as f:
+        f.writelines(error_causing_file)
+        f.flush()
+        tr = msprime.load_txt(f.name)
+    all_samples = tr.get_samples()
+    print('total samples produced:', len(all_samples))
+    # subset requires child of commit 21be37b in msprime
+    with pytest.raises(Exception) as e_info:
+        tr.subset(list(choice(all_samples, size=3, replace=False)))
+    print("drawing 10 samples with subset causes")
+    print(str(e_info))
+
+
+error_causing_file = """left	right	node	children	time	population
+0.690126	1.000000	16	0,1	0.000000	0
+0.000000	0.690126	15	0,1	0.000000	0
+0.244966	1.000000	8	0,2	0.000000	0
+0.000000	0.244966	7	0,2	0.000000	0
+0.280812	0.494669	28	12,16	0.500000	0
+0.494669	1.000000	28	12,14,16	0.500000	0
+0.166052	0.280812	28	0,12	0.500000	0
+0.000000	0.166052	27	12,14,16	0.500000	0
+0.166052	0.280812	27	14,16	0.500000	0
+0.280812	0.494669	27	0,14	0.500000	0
+0.000000	0.245273	26	8,10	0.500000	0
+0.245273	0.629274	26	0,8	0.500000	0
+0.629274	1.000000	25	8,10	0.500000	0
+0.245273	0.629274	25	0,10	0.500000	0
+0.000000	0.105372	24	3,5	0.500000	0
+0.105372	0.397841	24	0,5	0.500000	0
+0.397841	1.000000	23	3,5	0.500000	0
+0.105372	0.397841	23	0,3	0.500000	0
+0.000000	0.086988	22	4,6	0.500000	0
+0.086988	0.534236	22	0,6	0.500000	0
+0.534236	1.000000	21	4,6	0.500000	0
+0.086988	0.534236	21	0,4	0.500000	0
+0.000000	0.289003	20	11,13,15	0.500000	0
+0.289003	0.445229	20	11,15	0.500000	0
+0.445229	0.486477	20	0,11	0.500000	0
+0.445229	0.486477	19	13,15	0.500000	0
+0.486477	1.000000	19	11,13,15	0.500000	0
+0.289003	0.445229	19	0,13	0.500000	0
+0.214572	1.000000	32	7,9	0.500000	0
+0.202059	0.214572	32	0,7	0.500000	0
+0.000000	0.202059	31	7,9	0.500000	0
+0.202059	0.214572	31	0,9	0.500000	0
+0.000000	0.115878	46	18,20,22	1.000000	0
+0.115878	0.397743	46	18,20	1.000000	0
+0.397743	0.475430	46	0,20	1.000000	0
+0.397743	0.475430	45	18,22	1.000000	0
+0.475430	1.000000	45	18,20,22	1.000000	0
+0.115878	0.397743	45	0,22	1.000000	0
+0.000000	0.746958	44	17,19,21	1.000000	0
+0.746958	0.753220	44	17,21	1.000000	0
+0.753220	0.888258	44	0,21	1.000000	0
+0.753220	0.888258	43	17,19	1.000000	0
+0.888258	1.000000	43	17,19,21	1.000000	0
+0.746958	0.753220	43	0,19	1.000000	0
+0.000000	0.019253	40	24,26,28	1.000000	0
+0.019253	0.553969	40	24,26	1.000000	0
+0.553969	0.603567	40	0,26	1.000000	0
+0.553969	0.603567	39	24,28	1.000000	0
+0.603567	1.000000	39	24,26,28	1.000000	0
+0.019253	0.553969	39	0,28	1.000000	0
+0.590173	1.000000	38	30,32	1.000000	0
+0.528549	0.590173	38	0,32	1.000000	0
+0.000000	0.528549	37	30,32	1.000000	0
+0.528549	0.590173	37	0,30	1.000000	0
+0.000000	0.133154	36	23,25,27	1.000000	0
+0.133154	0.802394	36	23,25	1.000000	0
+0.802394	0.931816	36	0,23	1.000000	0
+0.802394	0.931816	35	25,27	1.000000	0
+0.931816	1.000000	35	23,25,27	1.000000	0
+0.133154	0.802394	35	0,27	1.000000	0
+0.528042	1.000000	34	29,31	1.000000	0
+0.203660	0.528042	34	0,31	1.000000	0
+0.000000	0.203660	33	29,31	1.000000	0
+0.203660	0.528042	33	0,29	1.000000	0
+0.143711	0.448333	61	35,37	1.500000	0
+0.448333	1.000000	61	35,37,39	1.500000	0
+0.079612	0.143711	61	0,35	1.500000	0
+0.000000	0.125499	60	41,43	1.500000	0
+0.125499	0.892115	60	0,43	1.500000	0
+0.892115	1.000000	59	41,43	1.500000	0
+0.125499	0.892115	59	0,41	1.500000	0
+0.731186	1.000000	58	0,33	1.500000	0
+0.000000	0.731186	57	0,33	1.500000	0
+0.000000	0.207413	56	0,46	1.500000	0
+0.207413	1.000000	55	0,46	1.500000	0
+0.248787	1.000000	54	0,34	1.500000	0
+0.000000	0.248787	53	0,34	1.500000	0
+0.359513	0.461162	52	38,40	1.500000	0
+0.461162	1.000000	52	36,38,40	1.500000	0
+0.192326	0.359513	52	0,40	1.500000	0
+0.000000	0.192326	51	36,38,40	1.500000	0
+0.192326	0.359513	51	36,38	1.500000	0
+0.359513	0.461162	51	0,36	1.500000	0
+0.000000	0.073946	50	42,44	1.500000	0
+0.073946	0.661872	50	0,42	1.500000	0
+0.661872	1.000000	49	42,44	1.500000	0
+0.073946	0.661872	49	0,44	1.500000	0
+0.985901	1.000000	48	0,45	1.500000	0
+0.000000	0.985901	47	0,45	1.500000	0
+0.000000	0.079612	62	35,37,39	1.500000	0
+0.079612	0.143711	62	37,39	1.500000	0
+0.143711	0.448333	62	0,39	1.500000	0
+0.062281	1.000000	92	0,48	2.000000	0
+0.000000	0.062281	91	0,48	2.000000	0
+0.000000	0.888176	88	0,49	2.000000	0
+0.888176	1.000000	87	0,49	2.000000	0
+0.000000	0.654026	86	60,62	2.000000	0
+0.654026	0.888698	86	0,60	2.000000	0
+0.888698	1.000000	85	60,62	2.000000	0
+0.654026	0.888698	85	0,62	2.000000	0
+0.296657	1.000000	84	0,50	2.000000	0
+0.000000	0.296657	83	0,50	2.000000	0
+0.912181	1.000000	78	0,58	2.000000	0
+0.000000	0.912181	77	0,58	2.000000	0
+0.211161	0.729941	76	52,56	2.000000	0
+0.729941	1.000000	76	52,54,56	2.000000	0
+0.081432	0.211161	76	0,52	2.000000	0
+0.000000	0.081432	75	52,54,56	2.000000	0
+0.081432	0.211161	75	54,56	2.000000	0
+0.211161	0.729941	75	0,54	2.000000	0
+0.132750	1.000000	74	0,47	2.000000	0
+0.000000	0.132750	73	0,47	2.000000	0
+0.000000	0.253049	68	0,57	2.000000	0
+0.253049	1.000000	67	0,57	2.000000	0
+0.000000	0.309377	66	51,53,55	2.000000	0
+0.309377	0.403999	66	53,55	2.000000	0
+0.403999	0.652236	66	0,53	2.000000	0
+0.403999	0.652236	65	51,55	2.000000	0
+0.652236	1.000000	65	51,53,55	2.000000	0
+0.309377	0.403999	65	0,51	2.000000	0
+0.000000	0.599859	64	59,61	2.000000	0
+0.599859	0.632942	64	0,61	2.000000	0
+0.632942	1.000000	63	59,61	2.000000	0
+0.599859	0.632942	63	0,59	2.000000	0
+"""
+
+if __name__=="__main__":
+    test_subset()
+
+