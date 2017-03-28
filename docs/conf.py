b3e2dec69c5510d72884b70605ec50fea391a51d
Jerome Kelleher <jk@well.ox.ac.uk>
2017-03-28 13:48:29 +0100

    Fix circular reference for building docs in Python 3.
    Fixes #158.

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ except ImportError:
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
-        return Mock()
+        return MagicMock()
 
 MOCK_MODULES = ["_msprime"]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
