d6c4595c9fc12952f4dc1f2f67c982911996a070
molpopgen <krthornt@uci.edu>
2017-08-21 08:28:40 -0700

    Do not use Mock. #240

--- docs/conf.py
+++ docs/conf.py
@@ -20,18 +20,18 @@ import shlex
 # documentation build on readthedocs, so we mock the module. Follows
 # the recommended pattern at
 # http://docs.readthedocs.org/en/latest/faq.html
-try:
-    from unittest.mock import MagicMock
-except ImportError:
-    from mock import Mock as MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-MOCK_MODULES = ["_msprime"]
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+# try:
+#     from unittest.mock import MagicMock
+# except ImportError:
+#     from mock import Mock as MagicMock
+# 
+# class Mock(MagicMock):
+#     @classmethod
+#     def __getattr__(cls, name):
+#         return MagicMock()
+# 
+# MOCK_MODULES = ["_msprime"]
+# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
