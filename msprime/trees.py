3d3c5206b6988facc5333d3c788b6f4b444be519
Jerome Kelleher <jk@well.ox.ac.uk>
2018-01-24 13:56:01 +0000

    Added node labels text & cleaned up API.
    Closes #367.

--- msprime/trees.py
+++ msprime/trees.py
@@ -668,10 +668,8 @@ class SparseTree(object):
         return self._ll_sparse_tree.get_sample_size()
 
     def draw(
-            self, path=None, width=None, height=None, times=False,
-            mutation_locations=True, mutation_labels=False,
-            internal_node_labels=True, leaf_node_labels=True, show_times=None,
-            node_label_text=None, format=None):
+            self, path=None, width=None, height=None,
+            node_labels=None, format=None):
         """
         Returns a representation of this tree in SVG format.
 
@@ -679,28 +677,17 @@ class SparseTree(object):
             write to file.
         :param int width: The width of the image in pixels.
         :param int height: The height of the image in pixels.
-        :param bool times: If True, show time labels at each internal node.
-        :param bool mutation_locations: If True, show mutations as points over nodes.
-        :param bool mutation_labels: If True, show labels for mutations.
-        :param bool internal_node_labels: If True, show labels for internal nodes.
-        :param bool leaf_node_labels: If True, show labels for leaf nodes.
-        :param map node_label_text: If specified, show custom labels for the nodes
+        :param map node_labels: If specified, show custom labels for the nodes
             that are present in the map. Any nodes not specified in the map will
-            have have their default labels.
+            have an empty string as the label.
         :param str format: The format of the returned image. Currently supported
             are 'svg', 'ascii' and 'unicode'.
-        :param bool show_times: Deprecated alias for ``times``.
         :return: A representation of this tree in SVG format.
         :rtype: str
         """
-        # show_times is a deprecated alias for times.
-        if show_times is not None:
-            times = show_times
         output = drawing.draw_tree(
-            self, format=format, width=width, height=height, times=times,
-            mutation_locations=mutation_locations, mutation_labels=mutation_labels,
-            internal_node_labels=internal_node_labels, leaf_node_labels=leaf_node_labels,
-            node_label_text=node_label_text)
+            self, format=format, width=width, height=height,
+            node_labels=node_labels)
         if path is not None:
             with open(path, "w") as f:
                 f.write(output)
