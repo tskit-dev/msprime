c0402f92c93f2fed90eac91415a16f31642ba5b6
Jerome Kelleher <jk@well.ox.ac.uk>
2018-04-17 21:14:57 +0100

    Code reorganisation to separate tskit from msprime.
    Creates a new header 'trees.h' which contains all the non simulator
    stuff from msprime.

--- lib/hapgen.c
+++ lib/hapgen.c
@@ -20,10 +20,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 
-#include "util.h"
-#include "object_heap.h"
-#include "msprime.h"
+#include "trees.h"
 
 
 /* Ensure the tree is in a consistent state */
