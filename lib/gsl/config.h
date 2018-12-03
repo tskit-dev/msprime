830156dcc74f89a1974261e8799771afecb3483a
Jerome Kelleher <jk@well.ox.ac.uk>
2018-12-03 17:09:47 +0000

    Initial addition of GSL files.

---
+++ lib/gsl/config.h
@@ -0,0 +1,12 @@
+/* NO-OP */
+
+#define HAVE_INLINE 1
+#define HAVE_IEEE_COMPARISONS 1
+#define RETURN_IF_NULL(x) if (!x) { return ; }
+#define HAVE_EXTENDED_PRECISION_REGISTERS 1
+
+#if HAVE_EXTENDED_PRECISION_REGISTERS
+#define GSL_COERCE_DBL(x) (gsl_coerce_double(x))
+#else
+#define GSL_COERCE_DBL(x) (x)
+#endif
