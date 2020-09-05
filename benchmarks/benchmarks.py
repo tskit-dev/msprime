4245436f7c9d0fad8839b630ac44060e1ec7e727
Jerome Kelleher <jk@well.ox.ac.uk>
2020-09-05 12:13:20 +0100

    Update ASV benchmarks.

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,7 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2018-2020 Tskit Developers
-# Copyright (c) 2015-2018 University of Oxford
+# Copyright (c) 2020 Tskit Developers
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -23,54 +22,92 @@
 """
 Benchmarks for msprime - use asv to run
 """
+import stdpopsim
+
 import msprime
 
-SAMPLE_SIZES = [2]  # , 1000, 10_000, 50_000, 100_000, 200_000]
 DEFAULTS = {
+    "sample_size": 1000,
     "length": 1e6,
-    "Ne": 1000,
+    "Ne": 10 ** 4,
     "recombination_rate": 1e-8,
-    "mutation_rate": 1e-8,
     "random_seed": 42,
 }
 
 
-class SimulateLength:
+class HudsonLength:
     version = "1"
-    param_names = ["sample_size", "length"]
-    params = [SAMPLE_SIZES, [10, 1e4, 1e5, 5e5, 1e6]]
+    param_names = ["length"]
+    params = [10 ** j for j in range(2, 8)]
 
-    def time_simulate_length(self, sample_size, length):
-        msprime.simulate(sample_size, **{**DEFAULTS, "length": length})
+    def time_hudson_length(self, length):
+        msprime.simulate(**{**DEFAULTS, "length": length})
 
 
-class SimulateNe:
+class HudsonSampleSize:
     version = "1"
-    param_names = ["sample_size", "Ne"]
-    params = [SAMPLE_SIZES, [1, 1e3, 2.5e3, 5e3, 1e4]]
+    param_names = ["sample_size"]
+    params = [10 ** j for j in range(1, 7)]
 
-    def time_simulate_Ne(self, sample_size, Ne):
-        msprime.simulate(sample_size, **{**DEFAULTS, "Ne": Ne})
+    def time_hudson_sample_size(self, sample_size):
+        msprime.simulate(**{**DEFAULTS, "sample_size": sample_size})
 
 
-class SimulateRecombination:
+class HumanRecombinationMapSuite:
     version = "1"
-    param_names = ["sample_size", "recombination_rate"]
-    params = [SAMPLE_SIZES, [0, 1e-9, 1e-8, 5e-8, 1e-7]]
 
-    def time_simulate_recombination(self, sample_size, recombination_rate):
+    def setup(self):
+        species = stdpopsim.get_species("HomSap")
+        genetic_map = species.get_genetic_map("HapMapII_GRCh37")
+        self.recomb_map_chr22 = genetic_map.get_chromosome_map("chr22")
+
+    def time_hudson_chr22(self):
         msprime.simulate(
-            sample_size, **{**DEFAULTS, "recombination_rate": recombination_rate}
+            100,
+            Ne=10 ** 4,
+            recombination_map=self.recomb_map_chr22,
+            random_seed=234,
+            end_time=1000,
+        )
+
+    def time_dtwf_chr22(self):
+        msprime.simulate(
+            100,
+            Ne=10 ** 4,
+            recombination_map=self.recomb_map_chr22,
+            random_seed=234,
+            model="dtwf",
+            end_time=1000,
         )
 
 
-class SimulateMutation:
-    version = "1"
-    param_names = ["sample_size", "mutation_rate"]
-    params = [SAMPLE_SIZES, [0, 1e-8, 1e-7, 1e-6, 1e-5]]
+# class SimulateNe:
+#     version = "1"
+#     param_names = ["sample_size", "Ne"]
+#     params = [SAMPLE_SIZES, [1, 1e3, 2.5e3, 5e3, 1e4]]
 
-    def time_simulate_mutation(self, sample_size, mutation_rate):
-        msprime.simulate(sample_size, **{**DEFAULTS, "mutation_rate": mutation_rate})
+#     def time_simulate_Ne(self, sample_size, Ne):
+#         msprime.simulate(sample_size, **{**DEFAULTS, "Ne": Ne})
+
+
+# class SimulateRecombination:
+#     version = "1"
+#     param_names = ["sample_size", "recombination_rate"]
+#     params = [SAMPLE_SIZES, [0, 1e-9, 1e-8, 5e-8, 1e-7]]
+
+#     def time_simulate_recombination(self, sample_size, recombination_rate):
+#         msprime.simulate(
+#             sample_size, **{**DEFAULTS, "recombination_rate": recombination_rate}
+#         )
+
+
+# class SimulateMutation:
+#     version = "1"
+#     param_names = ["sample_size", "mutation_rate"]
+#     params = [SAMPLE_SIZES, [0, 1e-8, 1e-7, 1e-6, 1e-5]]
+
+#     def time_simulate_mutation(self, sample_size, mutation_rate):
+#         msprime.simulate(sample_size, **{**DEFAULTS, "mutation_rate": mutation_rate})
 
 
 # class MemSuite:
