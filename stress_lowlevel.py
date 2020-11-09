4f7d52a0f1da85e5fde600a07e9f26a01c2043bf
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2020-11-09 11:18:07 +0000

    Fix stress script

--- stress_lowlevel.py
+++ stress_lowlevel.py
@@ -2,91 +2,99 @@
 Code to stress the low-level API as much as possible to expose
 any memory leaks or error handling issues.
 """
-import argparse
-import logging
+import curses
 import os
 import random
 import resource
 import sys
 import time
-import unittest
+import tracemalloc
+from contextlib import redirect_stdout
 
-import tests.test_ancestry as test_ancestry
-import tests.test_demography as test_demography
-import tests.test_dict_encoding as test_dict_encoding
-import tests.test_lowlevel as test_lowlevel
-import tests.test_recombination_map as test_recombination_map
+import pytest
 
 
-def main():
-    modules = {
-        "demography": test_demography,
-        "ancestry": test_ancestry,
-        "lowlevel": test_lowlevel,
-        "dict_encoding": test_dict_encoding,
-        "recombination_map": test_recombination_map,
-    }
-    parser = argparse.ArgumentParser(
-        description="Run tests in a loop to stress low-level interface"
-    )
-    parser.add_argument(
-        "-m",
-        "--module",
-        help="Run tests only on this module",
-        choices=list(modules.keys()),
-    )
-    args = parser.parse_args()
-    test_modules = list(modules.values())
-    if args.module is not None:
-        test_modules = [modules[args.module]]
+def main(stdscr):
+    if len(sys.argv) > 1:
+        args = sys.argv[1:]
+    else:
+        args = ["-n0", "tests/test_lowlevel.py"]
 
-    # Need to do this to silence the errors from the file_format tests.
-    logging.basicConfig(level=logging.ERROR)
+    class StressPlugin:
+        def __init__(self):
+            self.max_rss = 0
+            self.max_rss_iter = 0
+            self.min_rss = 1e100
+            self.iteration = 0
+            self.last_print = time.time()
+            self.memory_start = None
 
-    print("iter\ttests\terr\tfail\tskip\tRSS\tmin\tmax\tmax@iter")
-    max_rss = 0
-    max_rss_iter = 0
-    min_rss = 1e100
-    iteration = 0
-    last_print = time.time()
-    devnull = open(os.devnull, "w")
+        def pytest_sessionstart(self):
+            if self.memory_start is None:
+                tracemalloc.start()
+                self.memory_start = tracemalloc.take_snapshot()
+
+        def pytest_sessionfinish(self):
+            memory_current = tracemalloc.take_snapshot()
+            rusage = resource.getrusage(resource.RUSAGE_SELF)
+            if self.max_rss < rusage.ru_maxrss:
+                self.max_rss = rusage.ru_maxrss
+                self.max_rss_iter = self.iteration
+            if self.min_rss > rusage.ru_maxrss:
+                self.min_rss = rusage.ru_maxrss
+
+            # We don't want to flood stdout, so we rate-limit to 1 per second.
+            if time.time() - self.last_print > 1:
+                stdscr.clear()
+                rows, cols = stdscr.getmaxyx()
+                stdscr.addstr(
+                    0,
+                    0,
+                    "iter\tRSS\tmin\tmax\tmax@iter"[: cols - 1],
+                )
+                stdscr.addstr(
+                    1,
+                    0,
+                    "\t".join(
+                        map(
+                            str,
+                            [
+                                self.iteration,
+                                rusage.ru_maxrss,
+                                self.min_rss,
+                                self.max_rss,
+                                self.max_rss_iter,
+                            ],
+                        )
+                    )[: cols - 1],
+                )
+                stats = memory_current.compare_to(self.memory_start, "traceback")
+                for i, stat in enumerate(stats[: rows - 3], 1):
+                    stdscr.addstr(i + 2, 0, str(stat)[: cols - 1])
+                self.last_print = time.time()
+                stdscr.refresh()
+                self.iteration += 1
+
+    plugin = StressPlugin()
     while True:
         # We don't want any random variation in the amount of memory
         # used from test-to-test.
         random.seed(1)
-        testloader = unittest.TestLoader()
-        suite = testloader.loadTestsFromModule(test_modules[0])
-        for mod in test_modules[1:]:
-            suite.addTests(testloader.loadTestsFromModule(mod))
-        runner = unittest.TextTestRunner(verbosity=0, stream=devnull)
-        result = runner.run(suite)
-        rusage = resource.getrusage(resource.RUSAGE_SELF)
-        if max_rss < rusage.ru_maxrss:
-            max_rss = rusage.ru_maxrss
-            max_rss_iter = iteration
-        if min_rss > rusage.ru_maxrss:
-            min_rss = rusage.ru_maxrss
-
-        # We don't want to flood stdout, so we rate-limit to 1 per second.
-        if time.time() - last_print > 1:
-            print(
-                iteration,
-                result.testsRun,
-                len(result.failures),
-                len(result.errors),
-                len(result.skipped),
-                rusage.ru_maxrss,
-                min_rss,
-                max_rss,
-                max_rss_iter,
-                sep="\t",
-                end="\r",
-            )
-            last_print = time.time()
-            sys.stdout.flush()
-
-        iteration += 1
+        with open(os.devnull, "w") as devnull:
+            with redirect_stdout(devnull):
+                result = pytest.main(args, plugins=[plugin])
+        if result != 0:
+            exit("TESTS FAILED")
 
 
 if __name__ == "__main__":
-    main()
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+
+    try:
+        main(stdscr)
+    finally:
+        curses.echo()
+        curses.nocbreak()
+        curses.endwin()
