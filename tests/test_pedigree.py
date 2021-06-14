fb5e3597c6a4317c5b42e71a01d158f3a4a19afc
nbaya <nikolasbaya@gmail.com>
2021-06-14 16:37:10 +0100

    added simple pedigree simulator

--- tests/test_pedigree.py
+++ tests/test_pedigree.py
@@ -8,6 +8,7 @@ import pytest
 
 import msprime
 from msprime import pedigrees
+import tskit
 
 
 class TestPedigree(unittest.TestCase):
@@ -174,3 +175,170 @@ class TestPedigree(unittest.TestCase):
 
         with pytest.raises(ValueError):
             ped.get_times(individual=ped.individual)
+
+def simulate_pedigree(
+        tc: tskit.TableCollection,
+        n_founders = 8, 
+        n_children_prob = [0, 0, 2],
+        n_generations = 3,
+        random_sex=True):
+
+    def choose_sex(random=True, sex=None):
+        # male = 1, female = 2
+        if random:
+            sex = np.random.binomial(n=1, p=0.5) + 1
+        else: # non-random: alternate male female to ensure equal balance
+            sex = (sex % 2) + 1 
+        return sex
+    
+    def choose_n_children(n_children_prob, n_families=1):
+        return np.random.choice(
+            a=len(n_children_prob), 
+            size=n_families, 
+            p=n_children_prob)
+    
+    tb = tc.individuals
+    tb.metadata_schema = tskit.MetadataSchema(
+        {
+            "codec": "json",
+            "type": "object",
+            "properties": {
+                "sex": {"type": "integer"},
+            },
+            "required": ["sex"],
+            "additionalProperties": True,
+        }
+    )
+   
+    if random_sex:
+        sex=None
+    else:
+        sex = 1 # male=1, female=2
+    
+    # first generation
+    n_males = int(n_founders / 2)
+    curr_gen = [range(n_males), range(n_males, n_founders)]
+    for ind in range(n_founders):
+        tb.add_row(
+            parents=[-1, -1],
+            metadata={'sex': (ind>=n_males)+1}
+        )
+
+    # next generations            
+    for gen_idx in range(2, n_generations+1):
+        next_gen = [[], []]
+        avail_pat = np.random.permutation(curr_gen[0])
+        avail_mat = np.random.permutation(curr_gen[1])
+        n_pairs = min(len(curr_gen[0]), len(curr_gen[1]))
+        if n_pairs==0 and n_children_prob[0]!=1:
+            raise Exception(f"Not enough people to make children in generation {gen_idx}")
+        pairs = zip(avail_pat[:n_pairs], avail_mat[:n_pairs])
+        n_children_per_pair = choose_n_children(
+            n_children_prob=n_children_prob,
+            n_families=n_pairs
+        )
+        for (pat, mat), n_children in zip(pairs, n_children_per_pair):
+            for _ in range(n_children):
+                sex = choose_sex(
+                    random=random_sex, 
+                    sex=sex)
+                next_gen[sex-1] += [len(tb)]
+                tb.add_row(
+                    parents=[pat, mat],
+                    metadata={'sex': sex}
+                )
+        curr_gen = next_gen
+        
+class TestPedigreeSimulation():
+    """
+    Tests for simple pedigree simulator
+    """
+    def simple_sim(
+            self,
+            n_founders=2, 
+            n_children_prob=[0, 0, 1],
+            n_generations = 2,
+            random_sex=True):
+        tc=tskit.TableCollection(0)
+        simulate_pedigree(
+            tc=tc,
+            n_founders=n_founders, 
+            n_children_prob=n_children_prob,
+            n_generations=n_generations,
+            random_sex=random_sex)
+        return tc.individuals
+    
+    def test_one_generation_no_children(self):
+        n_founders = 8
+        tb = self.simple_sim(
+            n_founders=n_founders,
+            n_generations=1)
+        assert len(tb)==n_founders
+        
+        # check that all parents of founders are missing
+        assert all([np.array_equal(row.parents, [-1,-1]) for row in tb])
+
+        n_males = len([row for row in tb if row.metadata['sex']==1])
+        n_females = len([row for row in tb if row.metadata['sex']==2])
+        assert n_males==n_females==n_founders/2
+        
+    def test_one_trio(self):
+        tb = self.simple_sim(
+            n_founders=2,
+            n_children_prob=[0,1],
+            n_generations=2)
+        assert len(tb)==3
+        assert np.array_equal(tb[2].parents, [0,1])
+        assert all([np.array_equal(row.parents, [-1,-1]) for row in tb[:2]])
+        
+    def test_grandparents(self):
+        tb = self.simple_sim(
+            n_founders=4,
+            n_children_prob=[0,1],
+            n_generations=3,
+            random_sex=False)
+        assert len(tb)==7
+        assert {row.parents[0] for row in tb[4:6]}=={0,1}
+        assert {row.parents[1] for row in tb[4:6]}=={2,3}
+        assert all([np.array_equal(row.parents, [-1,-1]) for row in tb[:4]])        
+
+    def test_insufficient_founders(self):
+        with pytest.raises(Exception):
+            self.simple_sim(
+                n_founders=1,
+                n_children_prob=[0,1])
+        with pytest.raises(Exception):
+            self.simple_sim(
+                n_founders=3,
+                n_children_prob=[0,1],
+                n_generations=3)
+    
+    @pytest.mark.parametrize("n_children", range(5))
+    def test_nonrandom_child_prob(self, n_children):
+        tb = self.simple_sim(
+            n_founders=2,
+            n_children_prob=[0]*n_children+[1],
+            n_generations=2)
+        assert len(tb)==2+n_children
+        
+    @pytest.mark.parametrize("n_children_prob", [[0.5,0.5],
+                                                 [0,0.5,0.5]])
+    def test_expected_n_children(self, n_children_prob):
+        n_reps = 100
+        n_founders = 2
+        n_children = []
+        for _ in range(n_reps):
+            tb = self.simple_sim(
+                n_founders=n_founders,
+                n_children_prob=n_children_prob,
+                n_generations=2)
+            n_children.append(len(tb)-n_founders)
+        expected_n_children = sum(prob*n for prob, n in zip(n_children_prob, range(len(n_children_prob))))
+        assert np.mean(n_children) == pytest.approx(expected_n_children, rel=0.1)
+    
+    def test_bad_n_children_prob(self):
+        with pytest.raises(ValueError):
+            self.simple_sim(n_children_prob=[2])
+        with pytest.raises(ValueError):
+            self.simple_sim(n_children_prob=[1,1])
+        
