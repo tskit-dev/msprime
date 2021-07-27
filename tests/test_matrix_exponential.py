1eb2fa98a9f44c5b30d62f08337ea366121151ea
Graham Gower <graham.gower@gmail.com>
2021-07-27 10:15:31 +0200

    changes from review

--- tests/test_matrix_exponential.py
+++ tests/test_matrix_exponential.py
@@ -2,8 +2,6 @@ import textwrap
 
 import demes
 import numpy as np
-import pytest
-import scipy.linalg
 
 import msprime
 
@@ -28,19 +26,16 @@ class TestMatrixExponential:
     def verify(self, A, E=None):
         assert np.max(np.diag(A)) <= 0
         assert np.min(A - np.diag(np.diag(A))) >= 0
-        assert np.max(np.sum(A, 1)) <= 0
-        E1 = scipy.linalg.expm(A)
+        assert np.max(np.sum(A, 1)) <= 1e-10
+        E1 = _matrix_exponential_eigen(A)
         E2 = msprime.demography._matrix_exponential(A)
-        E3 = _matrix_exponential_eigen(A)
         assert np.min(E2) >= 0
         assert np.allclose(np.sum(E2, 1), np.ones((np.shape(A)[0],)))
-        assert E1.shape == E2.shape == E3.shape
+        assert E1.shape == E2.shape
         assert np.allclose(E1, E2)
-        assert np.allclose(E2, E3)
         if E is not None:
-            assert E.shape == E2.shape == E3.shape
+            assert E.shape == E2.shape
             assert np.allclose(E, E2)
-            assert np.allclose(E, E3)
 
     def test_zeros(self):
         for j in range(1, 10):
@@ -64,10 +59,26 @@ class TestMatrixExponential:
                 A[i, (i + 1) % k] = 1.0
             self.verify(A)
 
+    def test_diagonal_minus_row_sum(self):
+        for k in range(2, 5):
+            A = np.arange(k ** 2).reshape(k, k)
+            np.fill_diagonal(A, -1 * np.sum(A, 1) + np.diag(A))
+            self.verify(A)
+
+        rng = np.random.default_rng(1234)
+        for k in [2, 5, 30]:
+            for _ in range(20):
+                A = rng.uniform(size=(k, k))
+                np.fill_diagonal(A, -1 * np.sum(A, 1) + np.diag(A))
+                self.verify(A)
+
 
 class TestDemographyTrajectoriesEdgeCase:
-    @pytest.mark.slow
-    def test_edge_case(self, monkeypatch):
+    def test_edge_case(self):
+        # This test exposes unstable behaviour in some matrix exponential
+        # functions, namely scipy.linalg.expm() and _matrix_exponential_eigen()
+        # above. The instability leads to coalescence rate trajectories that
+        # have negative values and a probability vector that is non monotonic.
         # See issue #1775.
         model_str = textwrap.dedent(
             """\
@@ -95,43 +106,10 @@ class TestDemographyTrajectoriesEdgeCase:
         last_N = max(ddb.population_size_history[:, ddb.num_epochs - 1])
         last_epoch = ddb.epoch_start_time[-1]
         T = np.linspace(0, last_epoch + 12 * last_N, 101)
-        rates1, P1 = ddb.coalescence_rate_trajectory(
+        rates, probs = ddb.coalescence_rate_trajectory(
             T, {"A": 1, "C": 1}, double_step_validation=False
         )
-        assert np.all(rates1 >= 0)
-        assert np.all(P1 >= 0)
-        assert np.all(P1 <= 1)
-        assert np.all(np.diff(P1) <= 0)
-
-        # scipy.linalg.expm() is unstable for this example.
-        # Although results are similar to above, we get probabilities > 1.
-        monkeypatch.setattr(
-            msprime.demography, "_matrix_exponential", scipy.linalg.expm
-        )
-        rates2, P2 = ddb.coalescence_rate_trajectory(
-            T, {"A": 1, "C": 1}, double_step_validation=False
-        )
-        assert np.allclose(rates1, rates2)
-        assert np.allclose(P1, P2, rtol=1e-2)
-        assert np.all(rates2 >= 0)
-        assert np.all(P2 >= 0)
-        # Don't confirm failure here in case scipy improves in the future.
-        # with pytest.raises(AssertionError):
-        #    assert np.all(P2 <= 1)
-        # with pytest.raises(AssertionError):
-        #    assert np.all(np.diff(P2) <= 0)
-
-        # _matrix_exponential_eigen() is unstable for this example, and results
-        # are not even in the right ball park. The trajectory produces rates
-        # below zero and a probability vector that isn't close to monotonic.
-        monkeypatch.setattr(
-            msprime.demography, "_matrix_exponential", _matrix_exponential_eigen
-        )
-        rates3, P3 = ddb.coalescence_rate_trajectory(
-            T, {"A": 1, "C": 1}, double_step_validation=False
-        )
-        # Fails on Linux with openblas, but not on Github Action's MacOS blas.
-        # with pytest.raises(AssertionError):
-        #    assert np.all(rates3 >= 0)
-        with pytest.raises(AssertionError):
-            assert np.all(np.diff(P3) <= 0)
+        assert np.all(rates >= 0)
+        assert np.all(probs >= 0)
+        assert np.all(probs <= 1)
+        assert np.all(np.diff(probs) <= 0)
