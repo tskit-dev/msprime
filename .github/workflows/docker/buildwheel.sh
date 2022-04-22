f4c858b6e93548a3c408fa3d6efa73d25d8dc2e5
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2022-04-22 11:15:51 +0100

    Update to use the build module instead of setup.py directly
    This means that our build dependancies are explicity listed and we don't have to
    do tricks to get numpy to work.

--- .github/workflows/docker/buildwheel.sh
+++ .github/workflows/docker/buildwheel.sh
@@ -22,12 +22,11 @@ git config --global --add safe.directory /project
 git fetch --unshallow
 for V in "${PYTHON_VERSIONS[@]}"; do
     PYBIN=/opt/python/$V/bin
-    rm -rf build/       # Avoid lib build by narrow Python is used by wide python
-    # Instead of letting setup.py install a newer numpy we install it here
-    # using the oldest supported version for ABI compatibility
-    $PYBIN/pip install oldest-supported-numpy
-    $PYBIN/python setup.py build_ext --inplace
-    $PYBIN/python setup.py bdist_wheel
+    rm -rf build/       # Avoid lib build by one Python is used by another
+    $PYBIN/python -m venv env
+    source env/bin/activate
+    python -m pip install --upgrade build
+    SETUPTOOLS_SCM_DEBUG=1 python -m build
 done
 
 cd dist
