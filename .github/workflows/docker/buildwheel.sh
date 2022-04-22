4905ea0fe80fce1102667041e84a78697374a382
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2022-04-22 11:15:26 +0100

    Fix setuptools_scm not working with a shallow git clone in docker

--- .github/workflows/docker/buildwheel.sh
+++ .github/workflows/docker/buildwheel.sh
@@ -15,6 +15,11 @@ make -j 2
 make install
 cd ..
 
+# We're running as root in the docker container so git commands issued by
+# setuptools_scm will fail without this:
+git config --global --add safe.directory /project
+# Fetch the full history as we'll be missing tags otherwise.
+git fetch --unshallow
 for V in "${PYTHON_VERSIONS[@]}"; do
     PYBIN=/opt/python/$V/bin
     rm -rf build/       # Avoid lib build by narrow Python is used by wide python
