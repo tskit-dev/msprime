52abf6f9d6a553b1367990058331bc3b1c0b1a38
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2024-06-28 12:23:13 +0100

    Upgrade actions and use numpy2

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os.path
+import os
 import platform
 import subprocess
 from warnings import warn
@@ -24,7 +24,7 @@ class PathConfigurator:
         try:
             self._configure_gsl()
         except OSError as e:
-            warn(f"Error occured getting GSL path config: {e}")
+            warn(f"Error occurred getting GSL path config: {e}")
         # If the conda prefix is defined, then we are compiling in a conda
         # context. All include and lib paths should come from within this prefix.
         if CONDA_PREFIX is not None:
@@ -48,59 +48,62 @@ class PathConfigurator:
                 self.library_dirs.append(token[2:])
 
 
-libdir = "lib"
-tskroot = os.path.join("git-submodules", "tskit", "c")
-tskdir = os.path.join(tskroot, "tskit")
-kasdir = os.path.join(tskroot, "subprojects", "kastore")
-includes = ["lwt_interface", libdir, tskroot, tskdir, kasdir]
+def get_extension():
+    libdir = "lib"
+    tskroot = os.path.join("git-submodules", "tskit", "c")
+    tskdir = os.path.join(tskroot, "tskit")
+    kasdir = os.path.join(tskroot, "subprojects", "kastore")
+    includes = ["lwt_interface", libdir, tskroot, tskdir, kasdir]
 
-configurator = PathConfigurator()
-msp_source_files = [
-    "msprime.c",
-    "fenwick.c",
-    "avl.c",
-    "util.c",
-    "object_heap.c",
-    "rate_map.c",
-    "mutgen.c",
-    "likelihood.c",
-]
-tsk_source_files = ["core.c", "tables.c", "trees.c"]
-kas_source_files = ["kastore.c"]
-
-sources = (
-    ["msprime/_msprimemodule.c"]
-    + [os.path.join(libdir, f) for f in msp_source_files]
-    + [os.path.join(tskdir, f) for f in tsk_source_files]
-    + [os.path.join(kasdir, f) for f in kas_source_files]
-)
-
-libraries = ["gsl", "gslcblas"]
-defines = []
-if IS_WINDOWS:
-    # Needed for generating UUIDs
-    libraries.append("Advapi32")
-    defines += [
-        # These two are required for GSL to compile and link against the
-        # conda-forge version.
-        ("GSL_DLL", None),
-        ("WIN32", None),
+    configurator = PathConfigurator()
+    msp_source_files = [
+        "msprime.c",
+        "fenwick.c",
+        "avl.c",
+        "util.c",
+        "object_heap.c",
+        "rate_map.c",
+        "mutgen.c",
+        "likelihood.c",
     ]
+    tsk_source_files = ["core.c", "tables.c", "trees.c"]
+    kas_source_files = ["kastore.c"]
 
-_msprime_module = Extension(
-    "msprime._msprime",
-    sources=sources,
-    extra_compile_args=["-std=c99"],
-    libraries=libraries,
-    define_macros=defines,
-    include_dirs=includes + configurator.include_dirs + [numpy.get_include()],
-    library_dirs=configurator.library_dirs,
-)
+    sources = (
+        ["msprime/_msprimemodule.c"]
+        + [os.path.join(libdir, f) for f in msp_source_files]
+        + [os.path.join(tskdir, f) for f in tsk_source_files]
+        + [os.path.join(kasdir, f) for f in kas_source_files]
+    )
+
+    libraries = ["gsl", "gslcblas"]
+    defines = []
+    if IS_WINDOWS:
+        # Needed for generating UUIDs
+        libraries.append("Advapi32")
+        defines += [
+            # These two are required for GSL to compile and link against the
+            # conda-forge version.
+            ("GSL_DLL", None),
+            ("WIN32", None),
+        ]
+
+    return Extension(
+        "msprime._msprime",
+        sources=sources,
+        extra_compile_args=["-std=c99"],
+        libraries=libraries,
+        define_macros=defines,
+        include_dirs=includes + configurator.include_dirs + [numpy.get_include()],
+        library_dirs=configurator.library_dirs,
+    )
 
 
-setup(
-    # The package name along with all the other metadata is specified in setup.cfg
-    # However, GitHub's dependency graph can't see the package unless we put this here.
-    name="msprime",
-    ext_modules=[_msprime_module],
-)
+def main():
+    setup(
+        ext_modules=[get_extension()],
+    )
+
+
+if __name__ == "__main__":
+    main()
