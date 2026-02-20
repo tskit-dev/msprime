6fc416b5c0d1f05017ccc20f04d1b82be67ef66d
Jerome Kelleher <jk@well.ox.ac.uk>
2026-02-20 09:54:25 +0000

    Fully pin lint workflow

--- tests/test_dict_encoding.py
+++ tests/test_dict_encoding.py
@@ -1,9 +1,9 @@
 # Test cases for the low-level dictionary encoding used to move
 # data around in C.
 import lwt_interface.dict_encoding_testlib
-from msprime import _msprime
+import msprime
 
-lwt_interface.dict_encoding_testlib.lwt_module = _msprime
+lwt_interface.dict_encoding_testlib.lwt_module = msprime._msprime
 
 # Bring the tests defined in dict_encoding_testlib into the current namespace
 # so pytest will find and execute them.
