43b37cd2d59748fdcea37763011bd442f93cf138
Jerome Kelleher <jk@well.ox.ac.uk>
2026-02-27 15:09:12 +0000

    Use --group instead of --only-group in build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -6,7 +6,7 @@
 
 REPORTDIR=_build/html/reports
 
-uv run --project=.. --only-group docs jupyter-book build . -vnW --keep-going
+uv run --project=.. --group docs jupyter-book build . -vnW --keep-going
 RETVAL=$?
 if [ $RETVAL -ne 0 ]; then
     if [ -e $REPORTDIR ]; then
